9b35b24228dabfc0d0de2dae6cd091df829933ff
Andrew Rivero <andyyR134@gmail.com>
2023-09-27 00:31:52 -0700

    implementation/zoom video sdk

--- client/pages/zoom.js
+++ client/pages/zoom.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import MeetingStyles from "../src/styles/zoom.module.css";
 import router from "next/router";
-import middleWare from "../middleware";
+
 const axios = require("axios");
 
 const Meeting = () => {
@@ -23,8 +23,9 @@ const Meeting = () => {
           });
 
           let payload = router.query;
+
           const { data } = await axios({
-            url: middleWare,
+            url: "/api/Zoom",
             method: "post",
             data: payload,
           });
