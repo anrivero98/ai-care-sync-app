daca1f8a425247dcf9934592676579f2646823f6
Jesh Bheemanpally <jeshwanth@raverapp.com>
2023-10-15 19:47:36 -0700

    update frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Router } from "react-router-dom";
+import { Router, Routes } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -8,15 +8,26 @@ import { createHashHistory } from "history";
 
 const history = createHashHistory();
 
+
 ReactDOM.render(
   <React.StrictMode>
     <Router history={history}>
-      <App />
+      {/* <App /> */}
+      <TestApp />
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
 
+
+function TestApp() {
+
+  return (
+    <div className="App">
+      <h1>Hello!</h1>
+      </div>
+  )
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
